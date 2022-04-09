f487bb3db3a427a067c00cffb483fe199d6ce50f
Joel Chantrell <joelchantrell@gmail.com>
2022-04-09 13:18:38 +0930

    exercise 4.6 + 4.7

--- tests/listHelper.test.js
+++ tests/listHelper.test.js
@@ -1,5 +1,6 @@
-const { describe } = require('eslint/lib/rule-tester/rule-tester')
+
 const listHelper = require('../utils/listHelper')
+
 
 test('dummy returns one', () => {
     const blogs = []
@@ -89,5 +90,95 @@ describe('favourite blog', () => {
             __v: 0
         }
         )
+    })
+})
+
+describe('author who has the most blog entries', () => {
+    const noBlogs = []
+    const multipleBlogs = [
+        {
+            _id: '5a422aa71b54a676234d17f0',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Sally',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 3,
+            __v: 0
+        },
+        {
+            _id: '5a422aa71b54a676234d17f1',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Bob',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 5,
+            __v: 0
+        },
+        {
+            _id: '5a422aa71b54a676234d17f2',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Sally',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 10,
+            __v: 0
+        },
+        {
+            _id: '5a422aa71b54a676234d17f3',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Tim',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 10,
+            __v: 0
+        }]
+    test('if there are no blogs, there should be top poster', () => {
+        const result = listHelper.mostBlogs(noBlogs)
+        expect(result).toEqual({})
+    })
+    test('if author has the most blog entries, they should be the top poster', () => {
+        const result = listHelper.mostBlogs(multipleBlogs)
+        expect(result).toEqual({ author: 'Sally', blogs: 2 })
+    })
+})
+
+describe('author who has the most likes overall', () => {
+    const noBlogs = []
+    const multipleBlogs = [
+        {
+            _id: '5a422aa71b54a676234d17f0',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Sally',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 3,
+            __v: 0
+        },
+        {
+            _id: '5a422aa71b54a676234d17f1',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Bob',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 5,
+            __v: 0
+        },
+        {
+            _id: '5a422aa71b54a676234d17f2',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Sally',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 10,
+            __v: 0
+        },
+        {
+            _id: '5a422aa71b54a676234d17f3',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Tim',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 10,
+            __v: 0
+        }]
+    test('if there are no blogs, there should be no top author', () => {
+        const result = listHelper.mostLikes(noBlogs)
+        expect(result).toEqual({})
+    })
+    test('if author has the most likes overall, they should be returned', () => {
+        const result = listHelper.mostLikes(multipleBlogs)
+        expect(result).toEqual({ author: 'Sally', likes: 13 })
     })
 })
--- utils/listHelper.js
+++ utils/listHelper.js
@@ -1,7 +1,10 @@
+const { get } = require('lodash')
+const lodash = require('lodash')
+
+
 const dummy = (blogs) => {
     return 1
 }
-
 
 const totalLikes = (blogs) => {
     let total = 0
@@ -14,7 +17,6 @@ const totalLikes = (blogs) => {
         }))
     }
     return total
-
 }
 
 const favouriteBlog = (blogs) => {
@@ -31,12 +33,50 @@ const favouriteBlog = (blogs) => {
             favourite = favouriteBlog
         }))
     }
-    console.log(favourite)
     return favourite
+}
+
+const mostBlogs = (blogs) => {
+
+    const topPoster = {}
+
+    if (blogs.length === 0){
+        return {}
+    }
+    if (blogs.length >= 1){
+        const allBlogsByAuthor = lodash.map(blogs, 'author')
+        topPoster.author = lodash.chain(allBlogsByAuthor).countBy().entries().maxBy(lodash.last).head().value()
+        topPoster.blogs = lodash.chain(allBlogsByAuthor).countBy().entries().maxBy(lodash.last).nth(1).value()
+    }
+    return topPoster
+}
+
+const mostLikes = (blogs) => {
+    let topAuthor = {}
+
+    if (blogs.length === 0){
+        return {}
+    }
+    if (blogs.length >= 1){
+
+        const totalLikesByAuthor =
+        lodash(blogs)
+            .groupBy('author')
+            .map((objs, key) => ({
+                'author': key,
+                'likes': lodash.sumBy(objs, 'likes')
+            }))
+            .value()
+        topAuthor = totalLikesByAuthor.reduce((max, author) => max.likes > author.likes ? max : author)
+
+    }
+    return topAuthor
 }
 
 module.exports = {
     dummy,
     totalLikes,
-    favouriteBlog
+    favouriteBlog,
+    mostBlogs,
+    mostLikes
 }
